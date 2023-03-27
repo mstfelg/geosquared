ae8c1bd0544fb8b7c7b4a2def98ede1a1288dd84
mstfelg <me@mstfelg.com>
2023-03-27 16:48:24 -0700

    Move debugging options to main entry

--- desktop/src/main/java/org/geogebra/desktop/gui/app/GeoGebraFrame3D.java
+++ desktop/src/main/java/org/geogebra/desktop/gui/app/GeoGebraFrame3D.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import org.geogebra.desktop.CommandLineArguments;
 import org.geogebra.desktop.geogebra3D.App3D;
 import org.geogebra.desktop.main.AppD;
+import org.geogebra.desktop.main.GeoGebraPreferencesD;
 
 /**
  * Frame for geogebra 3D.
@@ -17,7 +18,17 @@ public class GeoGebraFrame3D extends GeoGebraFrame {
 	private static final long serialVersionUID = 1L;
 
 	public static synchronized void main(CommandLineArguments args) {
-		GeoGebraFrame.init(args, new GeoGebraFrame3D());
+		GeoGebraFrame wnd = new GeoGebraFrame();
+
+		// Set GeoGebraPreferences mode (system properties or property file)
+		// before it is called for the first time
+		String settingsFile = args.getStringValue("settingsfile");
+		if (settingsFile.length() > 0) {
+			GeoGebraPreferencesD.setPropertyFileName(settingsFile);
+		}
+
+		// create first window and show it
+		createNewWindow3D(args);
 	}
 
 	@Override
@@ -32,8 +43,7 @@ public class GeoGebraFrame3D extends GeoGebraFrame {
 	 *            command line arguments
 	 * @return new geogebra window
 	 */
-	public static synchronized GeoGebraFrame createNewWindow3D(
-			CommandLineArguments args) {
+	public static synchronized GeoGebraFrame createNewWindow3D(CommandLineArguments args) {
 		return createNewWindow(args, new GeoGebraFrame3D());
 	}
 
